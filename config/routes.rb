55cabee2b5f8bc67c0807f3d6597cb6de92c2317
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-04-08 06:28:54 +0000

    [Finish] 3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
   get "lists/new"
-  post "lists" => "lists#create" #以上2行追記
+  post "lists" => "lists#create" 
+  get "lists" => "lists#index"
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end
