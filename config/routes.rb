f590a526fbece58797eef3bea3ffd5469036182f
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-04-08 10:21:56 +0000

    3-5 error:editアクション, 編集ボタン

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "lists/new"
+  get "lists/:id/edit" => "lists#edit", as: "edit_list"
   post "lists" => "lists#create" 
   get "lists" => "lists#index"
+  patch "lists/:id" => "lists#update", as: "update_list"
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    
+  # get "up" => "rails/health#show", as: :rails_health_check
+  get "lists/:id" => "lists#show", as: "list"
 
   # Defines the root path route ("/")
   # root "posts#index"
